c350328fe9e98e44b30d3fd5ac79f986f6f932d0
Colin <colin.diesh@gmail.com>
2020-08-08 14:44:47 -0400

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Tree } from "phylo-react";
+import "./App.css";
 
 function App() {
+  const [tree, setTree] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={async () => {
+          setLoading(true);
+          const result = await fetch(
+            "/genetree/id/ENSGT00390000003602?prune_species=cow;prune_taxon=9526;content-type=text/x-nh;nh_format=simple"
+          );
+          if (!result.ok) {
+            setError(result.statusText);
+            setLoading(false);
+            return;
+          }
+          const text = await result.text();
+          setTree(text);
+          setError(undefined);
+          setLoading(false);
+        }}
+      >
+        <label>
+          Enter Ensembl gene ID
+          <input id="geneid" type="text" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {loading ? <p>Loading...</p> : null}
+      {error ? <p style={{ color: "red" }}>{error}</p> : null}
+      <Tree tree={tree} />
     </div>
   );
 }
